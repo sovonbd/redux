341a7e70ca5a83dff9b148cdb536f47d319c13eb
MR. Sovon <sovonsdp@gmail.com>
2024-07-23 13:28:26 -0500

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 function App() {
   return (
-    <div>
-      
+    <div className="h-screen w-full flex justify-center items-center">
+      <div className="flex border border-purple-300 rounded-md bg-slate-50 p-10">
+        <button className="px-3 py-2 rounded-md bg-green-500 text-white text-xl font-semibold">
+          Increment
+        </button>
+        <h1 className="text-3xl mx-10">0</h1>
+        <button className="px-3 py-2 rounded-md bg-red-500 text-white text-xl font-semibold">
+          Decrement
+        </button>
+      </div>
     </div>
   );
 }
